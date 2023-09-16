4a37cadee6c7fd39ace79c22f80652373444f5c7
keithro <rodriguez.keith@gmail.com>
2023-09-16 12:14:58 -0500

    Some basic setup and added daisyUI

---
+++ src/app/users/new/page.tsx
@@ -0,0 +1,9 @@
+const NewUserPage = () => {
+  return (
+    <div>
+      <h1>New User Page</h1>
+    </div>
+  );
+};
+
+export default NewUserPage;
---
+++ src/components/AddToCart.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const AddToCart = () => {
+  return (
+    <button className="btn btn-primary" onClick={() => console.log("Click")}>
+      Add To Cart
+    </button>
+  );
+};
+
+export default AddToCart;
